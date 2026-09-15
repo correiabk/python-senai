b6b8efa9813f33a5a6f7bb848a1fc77e6b20782c
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-15 14:53:18 -0300

    feat: Adicionando conteudo

---
+++ Definição-Literal/apresentacao-exercicio.py
@@ -0,0 +1,7 @@
+# Exercicio 01 - Apresentação
+nome = "Renan"
+idade = 24
+
+print(f"Olá, meu nome é {nome}")
+print(f"Tenho {idade} anos.")
+
---
+++ Definição-Literal/cartao-apresetacao.py
@@ -0,0 +1,12 @@
+# Cartão de Apresentação
+
+nome = "Renan"
+idade = 24
+cidade = "Cotia"
+profissao = "Desempregado"
+
+print(f"Meu nome é {nome}")
+print(f"Tenho {idade} anos.")
+print(f"Sou habitante de {cidade}")
+print(f"Atualmente estou {profissao}")
+
