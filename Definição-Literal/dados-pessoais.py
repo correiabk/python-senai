3cbc26fb83a8813e8ff55a212b44435d2ce517db
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-15 15:40:17 -0300

    feat: adicionando conteudo

---
+++ Definição-Literal/dados-pessoais.py
@@ -0,0 +1,17 @@
+# Execercicios  - Dados Pessoais
+nome = "Renan"
+idade = 24
+endereco = "Rua do Pilão"
+estudante = True
+
+print(f"Meu nome é {nome}")
+print(type(nome))
+
+print(f"Tenho {idade} anos")
+print(type(idade))
+
+print(f"Moro na {endereco}")
+print(type(endereco))
+
+print(f"É Estudante ? {estudante}")
+print(type(estudante))
