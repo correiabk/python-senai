d31cda30d4723844f29f976e26538d7b3064fe4e
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-15 16:37:34 -0300

    feat: adicionando conteudo

---
+++ Definição-Literal/exercicio-placar-futebol.py
@@ -0,0 +1,30 @@
+# Objetivo:
+# Praticar variáveis, números inteiros, operações matemáticas e f-strings.
+
+# Descrição:
+# Crie um programa que represente o resultado de uma partida de futebol.
+#  O programa deve armazenar o nome de dois times 
+#  e a quantidade de golos marcados por cada equipa.
+
+# Depois, apresente o placar e calcule o total de golos da partida.
+
+# Resultado no terminal
+# ================================
+#         RESULTADO DO JOGO
+# ================================
+# França 3 x 2 Espanha
+# Total de golos: 5
+# ================================
+
+time1 = "Franca"
+time2 = "Espanha"
+result1 = 3
+result2 = 2
+partida = 5
+
+print("============================")
+print("     RESULTADO DO JOGO      ")
+print("============================")
+print(f"{time1} {result1} x {result2} {time2}")
+print(f"Total de gols: {partida}")
+
