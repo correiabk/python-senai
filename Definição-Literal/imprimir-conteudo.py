41533f69edee8aa2fed7a3e714b737e29e94dead
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-15 13:32:14 -0300

    Realizando pequenas alterações

--- Definição-Literal/imprimir-conteudo.py
+++ Definição-Literal/imprimir-conteudo.py
@@ -2,7 +2,7 @@
 print ("Hoje tem semifinal: \nFrança vs Espanha")
 
 # Aqui tem 3 parametros 
-print ("Meu nome é " ,  "Thiago" , end='*')
+print ("Meu nome é " ,  "Renan" , end='*')
 
 # Separador no print 
-print ("Meu", "Nome", "é", "Thiago", sep= "-")
+print ("Meu", "Nome", "é", "Renan", sep= "-")
