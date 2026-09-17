7f6ac8f718ce3c509804ccbc59e6f0dc7a7232db
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-17 16:26:48 -0300

    feat: adicionando conteudo

--- operacao-de-entrada-saida/exercicio-calcular-idade.py
+++ operacao-de-entrada-saida/exercicio-calcular-idade.py
@@ -3,10 +3,9 @@
 
 # Entrada de dados
 anonasc = int(input("Insira o seu ano de nascimento: "))
-anoatu = int(input("Insira o ano atual: "))
 
 # Processamento de dados
-idadeat = anoatu - anonasc
+idadeat = 2026 - anonasc
 
 # Saída de informações
 print(F"Você nasceu no ano de {anonasc} e tem {idadeat} anos")
