60837e4167825915c8aa554801edc18f180b4305
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-17 14:26:08 -0300

    feat: exercicio de variacao matematica

---
+++ operacao-de-entrada-saida/exercicio-dobro-numero.py
@@ -0,0 +1,11 @@
+# Faça um programa que peça números ao input
+# E mostre o dobro desse némero
+
+# Entrada de dados 
+n1 = int(input("Digite um número: "))
+
+# Processamento computacional
+soma = n1 * 2
+
+# Saída das informações
+print(f"O dobro do seu numero é: {soma}")
