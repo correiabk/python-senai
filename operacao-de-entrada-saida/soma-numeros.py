502d9ce56d23c5d71fa4946cc371999a3a480502
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-17 14:01:55 -0300

    feat: adicionando material

---
+++ operacao-de-entrada-saida/soma-numeros.py
@@ -0,0 +1,15 @@
+# Entrada de dados básicos
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+# Processamento computacional
+soma = numero1 + numero2
+sub = numero1 - numero2
+divs = numero1 / numero2
+mul = numero1 * numero2
+
+# Saída da informação
+print(f"A soma é: {soma}")
+print(f"A subtração é: {sub}")
+print(f"A divisão é: {divs}")
+print(f"A multiplicação é: {mul}")
