fc8eaf05f941df331462263ecccfff21f6b1267b
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-22 16:40:03 -0300

    feat: exercicio de listas

---
+++ 05-listas/06-lista-agrupadas.py
@@ -0,0 +1,10 @@
+filmes = [
+    ["Batman", "Ação"],
+    ["Titanic", "Romance"],
+    ["Cidade de Deus", "Tony Story"]
+]
+
+# Cada filme agrupada com seu genêro
+print(filmes[0]) #1 linha inteira
+print(filmes[0][0]) #Somente o Batman
+print(filmes[0][1]) #Somenete a ação
