62748271b8eb865a035e6ebfce747c356f869ab2
RENAN CORREIA SANTOS <renan.santos127@senaisp.edu.br>
2026-09-22 14:55:31 -0300

    feat: adicionando material

---
+++ 05-listas/04-exercico-verificar-time-futebol.py
@@ -0,0 +1,21 @@
+# Crie um programa em Python que tenha uma lista com alguns times de futebol.
+
+# Peça para o usuário digitar o nome de um time.
+
+# Verifique se o time digitado está na lista.
+
+# Se estiver, mostre:
+# "Esse time está na lista!"
+
+# Caso contrário, mostre:
+# "Esse time não está na lista!"
+
+
+times = ["Corinthians", "Palmeiras", "Santos", "São Paulo"]
+
+team = input("Insira o nome do sue time em São Paulo: ")
+
+if team in times:
+    print(f"{team} está na lista!")
+else:
+    print(f"{team} não está na lista!")
